af90cd3575ddd5a6899484e6b19357c46342762a
Julie Vaccalluzzo <julie.vaccalluzzo@capgemini.com>
2025-12-19 10:53:00 +0000

    update help links and pass in document number

--- app/components/helpLink.tsx
+++ app/components/helpLink.tsx
@@ -11,30 +11,22 @@ export const HelpLink = () => {
   return (
     <div className="govuk-!-margin-bottom-6 govuk-!-margin-top-8 govuk-!-margin-bottom-6" data-testid="help-section">
       <hr className="horizontal-line" />
-      <h2 className="govuk-heading-l" data-test-id="get-help-heading">
-        {t("getHelpHeading")}
-      </h2>
-      <p className="govuk-body" data-test-id="get-help-body">
-        {t("getHelpBody", { documentNumber })}
-      </p>
+      <h2 className="govuk-heading-l">{t("getHelpHeading")}</h2>
+      <p className="govuk-body">{t("getHelpBody", { documentNumber })}</p>
       <p className="govuk-body">
-        <span data-test-id="get-help-phone">{t("getHelpPhone")}</span>
+        {t("getHelpPhone")}
         <br />
-        <span data-test-id="get-help-hours"> {t("getHelpHours")}</span>
+        {t("getHelpHours")}
       </p>
       <p className="govuk-body">
         <a data-test-id="call-charges-link" className="govuk-link" href="https://www.gov.uk/call-charges">
           {t("callChargesText")}
         </a>
       </p>
-      <p className="govuk-body" data-test-id="get-help-guidance">
-        {t("getHelpReadGuidance")}&nbsp;
-        <a
-          data-test-id="exporting-link"
-          className="govuk-link"
-          href="https://www.gov.uk/guidance/exporting-or-moving-fish-from-the-uk#get-help-with-fish-export-documents"
-        >
-          {t("getHelpMovingFish")}&nbsp;
+      <p className="govuk-body">
+        {t("getHelpReadGuidance")} &nbsp;
+        <a data-test-id="call-charges-link" className="govuk-link" href="https://www.gov.uk/call-charges">
+          {t("getHelpMovingFish")} &nbsp;
           {t("commonHelpLinkOpenInNewTab")}
         </a>
       </p>
