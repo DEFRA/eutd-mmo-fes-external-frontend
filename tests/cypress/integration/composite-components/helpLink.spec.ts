6fab4de03f1ae1e598cb7e7dba36ffa557ec09b3
Vaccalluzzo, Julie (Guest) <julie.vaccalluzzo@capgemini.com>
2025-12-24 13:18:01 +0000

    Merged PR 98593: Update get help section across service
    Please include a summary of the change:
    - Updated the help link section
    - Updated the copy for both Welsh and English, removed unused copy
    - Added specific tests for Help section
     - Removed redundant test for 'addConsignmentDetails.spec.ts' covered by 'helpLink.spec.ts'
    
    Fixes - Jira Link #
    - https://eaflood.atlassian.net/browse/FI0-10520
    
    Please tick the relevant option for your PR request. Follow Semantic Version: v`MAJOR.MINOR.PATCH`
    
    - [ ] Bug fix - PATCH (non-breaking change which fixes an issue)
    - [x] New feature - MINOR (non-breaking change which adds functionality)
    - [ ] Breaking change - MAJOR (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update, or other non-code files.
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] I have added tests that prove my fix is effective or that my feature works
    - [x] New and existing unit tests pass locally with my changes

--- tests/cypress/integration/composite-components/helpLink.spec.ts
+++ tests/cypress/integration/composite-components/helpLink.spec.ts
@@ -1,75 +1,66 @@
-import { TestCaseId } from "~/types";
+import { type ITestParams, TestCaseId } from "~/types";
 
 describe("HelpSection", () => {
-  const progressCases = [
-    {
-      url: "/create-catch-certificate/GBR-2022-CC-488FE89C1/progress",
-      documentName: "Catch Certificate",
-      testCaseId: TestCaseId.CCUploadEntryCompleteProgress,
-    },
-    {
-      url: "/create-processing-statement/GBR-2022-CC-488FE89C1/progress",
-      documentName: "Processing Statement",
-      testCaseId: TestCaseId.PSCompleteProgress,
-    },
-    {
-      url: "/create-storage-document/GBR-2022-CC-488FE89C1/progress",
-      documentName: "Non-Manipulation Document",
-      testCaseId: TestCaseId.SDCompleteProgress,
-    },
-  ];
+  it("displays help section on three types of pages", () => {
+    const urls = [
+      "/create-catch-certificate/GBR-2022-CC-488FE89C1/progress",
+      "/create-processing-statement/GBR-2022-CC-488FE89C1/progress",
+      "/create-storage-document/GBR-2022-CC-488FE89C1/progress",
+    ];
 
-  progressCases.forEach(({ url, documentName, testCaseId }) => {
-    it(`displays help section and document documentName for ${documentName}`, () => {
-      cy.visit(url, { qs: { testCaseId } });
-      cy.get("[data-testid=help-section]").should("exist");
-      cy.get(".govuk-heading-l").should("contain.text", "Get help with your application");
-      cy.get("[data-test-id=get-help-body]").should(
-        "contain.text",
-        `If you have a ${documentName} unique reference number, have it with you when you call.`
-      );
-      cy.get("[data-test-id=get-help-phone]").should("contain.text", "Phone: 0330 159 198");
-      cy.get("[data-test-id=get-help-hours]").should(
-        "contain.text",
-        "Monday to Friday, 9am to 5pm (except public holidays)"
-      );
-      cy.get("[data-test-id=call-charges-link]").should("have.attr", "href", "https://www.gov.uk/call-charges");
-      cy.get("[data-test-id=call-charges-link]").should("contain.text", "Find out about call charges");
-      cy.get("[data-test-id=get-help-guidance]").should("contain.text", "Read the guidance on");
-      cy.get("[data-test-id=exporting-link]").should(
-        "have.attr",
-        "href",
-        "https://www.gov.uk/guidance/exporting-or-moving-fish-from-the-uk#get-help-with-fish-export-documents"
-      );
-      cy.get("[data-test-id='exporting-link']")
-        .invoke("text")
-        .should("match", /exporting or moving fish from the UK\s*\(opens in new tab\)/i); // normalise whitespace
+    urls.forEach((url) => {
+      it(`Should display tHe help section on ${url}`, () => {
+        cy.visit(url);
+        cy.get("[data-testid=help-section]").should("exist");
+      });
     });
   });
 
   it("does not display on the following types of pages", () => {
-    const dashboardCases = [
-      {
-        url: "/create-catch-certificate/catch-certificates",
-        testCaseId: TestCaseId.CCDashboard,
-      },
-      {
-        url: "/create-processing-statement/processing-statements",
-        testCaseId: TestCaseId.PSDashboard,
-      },
-      {
-        url: "/create-storage-document/storage-documents",
-        testCaseId: TestCaseId.SDDashboard,
-      },
-      {
-        url: "/manage-favourites",
-        testCaseId: TestCaseId.ManageFavourites,
-      },
+    const urls = [
+      "/create-catch-certificate/catch-certificates",
+      "/create-processing-statement/processing-statements",
+      "/create-storage-document/storage-documents",
+      "/manage-favourites",
     ];
 
-    dashboardCases.forEach(({ url, testCaseId }) => {
-      cy.visit(url, { qs: { testCaseId } });
-      cy.get("[data-testid=help-section]").should("not.exist");
+    urls.forEach((url) => {
+      it(`Should not display the help section on ${url}`, () => {
+        cy.visit(url);
+        cy.get("[data-testid=help-section]").should("exist");
+      });
     });
+  });
+
+  it("displays help section contents", () => {
+    const testParams: ITestParams = {
+      testCaseId: TestCaseId.CCUploadEntryCompleteProgress,
+    };
+
+    const url = "/create-catch-certificate/GBR-2022-CC-488FE89C1/progress";
+
+    cy.visit(url, { qs: { ...testParams } });
+
+    cy.get(".govuk-heading-l").should("contain.text", "Get help with your application");
+    cy.get("[data-test-id=get-help-body]").should(
+      "contain.text",
+      "If you have a GBR-2022-CC-488FE89C1 unique reference number, have it with you when you call."
+    );
+    cy.get("[data-test-id=get-help-phone]").should("contain.text", "Phone: 0330 159 198");
+    cy.get("[data-test-id=get-help-hours]").should(
+      "contain.text",
+      "Monday to Friday, 9am to 5pm (except public holidays)"
+    );
+    cy.get("[data-test-id=call-charges-link]").should("have.attr", "href", "https://www.gov.uk/call-charges");
+    cy.get("[data-test-id=call-charges-link]").should("contain.text", "Find out about call charges");
+    cy.get("[data-test-id=get-help-guidance]").should("contain.text", "Read the guidance on");
+    cy.get("[data-test-id=exporting-link]").should(
+      "have.attr",
+      "href",
+      "https://www.gov.uk/guidance/exporting-or-moving-fish-from-the-uk#get-help-with-fish-export-documents"
+    );
+    cy.get("[data-test-id='exporting-link']")
+      .invoke("text")
+      .should("match", /exporting or moving fish from the UK\s*\(opens in new tab\)/i); // normalise whitespace
   });
 });
