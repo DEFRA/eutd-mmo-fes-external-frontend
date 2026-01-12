8732d31f0325a3f2387011c719785db5041e3e22
Ayush Watnani <watnaniayush@gmail.com>
2026-01-12 13:10:32 +0530

    Feat/fi0 9868 (#27)
    * replace all references of 'storage document' with 'non-manipulation document' on check-your-info page
    
    * resolved test cases
    
    * resolved failing test case - 02

--- tests/cypress/integration/routes/storageDocumentCreated.spec.ts
+++ tests/cypress/integration/routes/storageDocumentCreated.spec.ts
@@ -1,6 +1,6 @@
 import { type ITestParams, TestCaseId } from "~/types";
 const documentNumber = "GBR-2023-SD-97DA962EC";
-const storageDocumentUrl = `/create-non-manipulation-document/${documentNumber}/non-manipulation-document-created`;
+const storageDocumentUrl = `/create-storage-document/${documentNumber}/storage-document-created`;
 
 describe("Storage document created page: rendering", () => {
   beforeEach(() => {
@@ -12,10 +12,10 @@ describe("Storage document created page: rendering", () => {
   });
 
   it("should render links", () => {
-    cy.contains("strong", /^Download the non-manipulation document$/).should("be.visible");
-    cy.contains("a", /^View completed non-manipulation documents or create a new export document$/)
+    cy.contains("strong", /^Download the storage document$/).should("be.visible");
+    cy.contains("a", /^View completed storage document or create a new export document$/)
       .should("be.visible")
-      .should("have.attr", "href", "/create-non-manipulation-document/non-manipulation-documents");
+      .should("have.attr", "href", "/create-storage-document/storage-documents");
   });
 
   it("should render links for user satisfaction survey feedback link for storage document", () => {
@@ -26,10 +26,7 @@ describe("Storage document created page: rendering", () => {
   });
 
   it("should render the correct page title and subtitle", () => {
-    cy.title().should(
-      "eq",
-      "The non-manipulation document has been created - Create a UK non-manipulation document - GOV.UK"
-    );
+    cy.title().should("eq", "The non-manipulation document has been created - Create a UK storage document - GOV.UK");
     cy.findByRole("heading", { name: "The non-manipulation document has been created", level: 1 });
     cy.get(".govuk-panel__body").contains("Non-manipulation document number");
     cy.get(".govuk-panel__body").contains(documentNumber);
@@ -37,7 +34,7 @@ describe("Storage document created page: rendering", () => {
 
   it("should render all step-by-step instructions with correct text", () => {
     cy.get(".govuk-heading-m").contains("What you need to do next").should("be.visible");
-    cy.contains("strong", "Download the non-manipulation document").should("be.visible");
+    cy.contains("strong", "Download the storage document").should("be.visible");
     cy.contains(
       "li",
       "If you are using Firefox as an internet browser, please ensure JavaScript is enabled in order to view and download the certificate"
@@ -45,7 +42,7 @@ describe("Storage document created page: rendering", () => {
     cy.contains("li", "If you are using a mobile device, please ensure you have installed a PDF viewer").should(
       "be.visible"
     );
-    cy.contains("strong", "Email the non-manipulation document to the importer.").should("be.visible");
+    cy.contains("strong", "Email the storage document to the importer.").should("be.visible");
     cy.contains(
       "li",
       "It is the importer’s responsibility to submit it to the import control authority where your export will enter the EU."
@@ -54,20 +51,20 @@ describe("Storage document created page: rendering", () => {
       "li",
       "The importing authority will complete and sign their section of the document at the Border Inspection Post (BIP)."
     ).should("be.visible");
-    cy.contains("View completed non-manipulation documents or create a new export document").should("be.visible");
+    cy.contains("View completed storage document or create a new export document").should("be.visible");
   });
 
   it("should render important notice with exclamation icon", () => {
     cy.get('svg[viewBox="0 0 35.000000 35.000000"]').should("be.visible");
     cy.get("svg title").contains("icon important").should("exist");
-    cy.contains("strong", "Do not amend the non-manipulation document.").should("be.visible");
+    cy.contains("strong", "Do not amend the storage document.").should("be.visible");
   });
 
   it("should link to the storage document dashboard", () => {
     cy.findByRole("link", {
-      name: "View completed non-manipulation documents or create a new export document",
+      name: "View completed storage document or create a new export document",
     }).click({ force: true });
-    cy.url().should("include", "/create-non-manipulation-document/non-manipulation-documents");
+    cy.url().should("include", "/create-storage-document/storage-documents");
   });
 
   it("should render survey component", () => {
@@ -82,6 +79,6 @@ describe("Storage document created page: pageguard", () => {
       args: [documentNumber],
     };
     cy.visit(storageDocumentUrl, { failOnStatusCode: false, qs: { ...testParams } });
-    cy.url().should("include", "/create-non-manipulation-document/non-manipulation-documents");
+    cy.url().should("include", "/create-storage-document/storage-documents");
   });
 });
