ed03ad16d757b7d3ae86f397583e89264fc6e5bf
Senthilkumar T <senthilkumar.t@Defra.onmicrosoft.com>
2026-01-12 13:03:43 +0000

    FI0-10559 Sonar Cloud errors across all repositories

--- app/routes/create-processing-statement.$documentNumber.add-catch-details.$.tsx
+++ app/routes/create-processing-statement.$documentNumber.add-catch-details.$.tsx
@@ -1,7 +1,5 @@
 import AddCatchDetailsIndex from "~/routes/create-processing-statement.$documentNumber.add-catch-details._index";
-import { loader, action } from "./create-processing-statement.$documentNumber.add-catch-details._index";
-export { loader };
-export { action };
+export { loader, action } from "./create-processing-statement.$documentNumber.add-catch-details._index";
 
 /* istanbul ignore next */
 export default function AddCatchDetails() {
