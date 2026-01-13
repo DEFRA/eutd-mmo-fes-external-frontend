1d217db90f62621cf05209945e230b182fb2d152
Senthilkumar T <mailtosenthil91@gmail.com>
2026-01-13 13:44:32 +0530

    Fo 0-10559 Sonar Cloud errors across all repositories (#32)
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    * FI0-10559 Sonar Cloud errors across all repositories
    
    ---------
    
    Co-authored-by: Senthilkumar T <senthilkumar.t@Defra.onmicrosoft.com>

---
+++ app/routes/create-storage-document.$documentNumber.add-product-to-this-consignment.$.tsx
@@ -0,0 +1,8 @@
+import AddProductIndex from "~/routes/create-storage-document.$documentNumber.add-product-to-this-consignment._index";
+
+export { loader, action } from "./create-storage-document.$documentNumber.add-product-to-this-consignment._index";
+
+/* istanbul ignore next */
+export default function AddProduct() {
+  return <AddProductIndex />;
+}
