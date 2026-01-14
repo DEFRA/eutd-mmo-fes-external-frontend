8c6b1ef3a4abdc34d0c07284c790d8a88abf21d6
Shemsedin Callaki <shemsedin.callaki@capgemini.com>
2026-01-14 11:05:35 +0000

    Enhance catch handling: add support for products with no catches and update tests

--- app/types/tests.ts
+++ app/types/tests.ts
@@ -588,6 +588,7 @@ export enum TestCaseId {
   PSCatchAddedFromProgressNoCatchDetails = "psCatchAddedFromProgressNoCatchDetails",
   PSCatchAddedFromProgressNoCatchDetails1 = "psCatchAddedFromProgressNoCatchDetails1",
   PSCatchAddedFromProgressNoCatchWeights = "psCatchAddedFromProgressNoCatchWeights",
+  PSCatchAddedTwoProductsOnlyOneWithCatches = "psCatchAddedTwoProductsOnlyOneWithCatches",
   PSCatchAddedBlankOneCatch = "psCatchAddedBlankOneCatch",
   PSCatchAddedNoCatches = "psCatchAddedNoCatches",
   PSCatchAddedTwoCatches = "psCatchAddedTwoCatches",
