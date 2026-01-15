c6994d8153736583b2237bda4e8a68c9531544af
Shemsedin Callaki <shemsedin.callaki@capgemini.com>
2026-01-15 17:51:48 +0000

    Refactor catchAdded tests to improve search result validation and enhance catchAddedHandler with additional POST handlers

--- tests/cypress/integration/routes/doYouHaveARoadTransportDocument.spec.ts
+++ tests/cypress/integration/routes/doYouHaveARoadTransportDocument.spec.ts
@@ -54,10 +54,11 @@ describe("DoYouHaveARoadTransportDocument", () => {
     // .should("be.visible") does not seem to do the job
     cy.wait(250);
 
-    cy.get('input[type="radio"][value="true"]').check();
+    cy.get('input[type="radio"][value="true"]').should("be.visible").check({ force: true });
+    cy.wait(200); // Allow state to update after radio selection
     cy.get("[data-testid=save-and-continue").click({ force: true });
 
-    cy.url().should("include", "/do-you-have-additional-transport-types");
+    cy.url({ timeout: 10000 }).should("include", "/do-you-have-additional-transport-types");
   });
 
   it("should redirect user to transportation details page when user submits no", () => {
