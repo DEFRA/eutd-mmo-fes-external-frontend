7fa7fb60a245b73a52860a3857a52d344a308551
Maneesh2-f <maneeshyadav108@gmail.com>
2026-01-15 09:04:59 +0000

    Fix DoYouHaveARoadTransportDocument redirect logic
    - Add cmr field to onUpdateTransport response
    - Change redirect condition to check response.cmr instead of payload.cmr
    - Remove unnecessary visibility check in Cypress test

--- app/models/doYouAHaveRoadTransportDocument.server.ts
+++ app/models/doYouAHaveRoadTransportDocument.server.ts
@@ -120,7 +120,7 @@ export const DoYouAHaveRoadTransportDocumentAction = async (
     return apiCallFailed(errors, values);
   }
 
-  if (payload.cmr === "true") {
+  if (response.cmr === "true") {
     return redirect(
       route("/create-catch-certificate/:documentNumber/do-you-have-additional-transport-types", { documentNumber })
     );
