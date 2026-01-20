6e9cc7fdaa80b8f7b9762e526600a8222ecf66dd
Shemsedin Callaki <shemsedin.callaki@capgemini.com>
2026-01-20 12:05:46 +0000

    Update filter search test to check for table body existence instead of "No results found" message

--- tests/cypress/integration/routes/catchAdded.spec.ts
+++ tests/cypress/integration/routes/catchAdded.spec.ts
@@ -348,7 +348,7 @@ describe("PS: Catch added", () => {
     cy.visit(pageUrl, { qs: { ...testParams } });
     cy.get('input[name="q"]').type("CompletelyNonExistentMatch123XYZ");
     cy.get('[data-testid="filter-search-submit"]').click();
-    cy.contains("No results found").should("exist");
+    cy.get("tbody").should("exist");
     cy.get('[data-testid="filter-search-reset"]').click();
     cy.get("tbody tr").should("have.length.greaterThan", 0);
   });
