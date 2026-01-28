f15cfbc91c7cf75dcfb555a5f29a6b393122f8bb
Shemsedin Callaki <shemsedin.callaki@capgemini.com>
2026-01-28 09:51:26 +0000

    Remove exclusive test run for navigation to storage facility page in validation scenarios

--- tests/cypress/integration/routes/storageDocumentAddArrivalTransportationDetailsContainerVessel.spec.ts
+++ tests/cypress/integration/routes/storageDocumentAddArrivalTransportationDetailsContainerVessel.spec.ts
@@ -428,7 +428,7 @@ describe("Container Vessel Arrival Required Fields Validation", () => {
     cy.contains("a", /^Enter the departure date$/).should("be.visible");
   });
 
-  it.only("should navigate to storage facility page when all mandatory fields are populated", () => {
+  it("should navigate to storage facility page when all mandatory fields are populated", () => {
     cy.visit(addArrivalTransportationDetailsContainerVesselUrl, { qs: { ...testParams } });
     cy.get("#vesselName").should("be.visible").clear();
     cy.get("#vesselName").type("Test Vessel", { force: true });
