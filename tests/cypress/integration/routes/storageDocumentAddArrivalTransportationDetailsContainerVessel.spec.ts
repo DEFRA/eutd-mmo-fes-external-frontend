37b78c1948fbff0061e6812a72476a07f0fa8fe7
Mo Nashawati <mo.nashawati@capgemini.com>
2026-01-28 10:19:27 +0000

    fix test

--- tests/cypress/integration/routes/storageDocumentAddArrivalTransportationDetailsContainerVessel.spec.ts
+++ tests/cypress/integration/routes/storageDocumentAddArrivalTransportationDetailsContainerVessel.spec.ts
@@ -430,7 +430,9 @@ describe("Container Vessel Arrival Required Fields Validation", () => {
 
   it("should navigate to storage facility page when all mandatory fields are populated", () => {
     cy.visit(addArrivalTransportationDetailsContainerVesselUrl, { qs: { ...testParams } });
-    cy.get("#vesselName").should("be.visible").type("Test Vessel", { force: true });
+    cy.get("#vesselName").should("be.visible").clear();
+    cy.get("#vesselName").type("Test Vessel", { force: true });
+    cy.get("#vesselName").should("have.value", "Test Vessel");
     cy.get("#flagState").should("be.visible").type("Greece", { force: true });
     cy.get('[id="containerNumbers.0"]').should("be.visible").type("ABCD1234567", { force: true });
     cy.get("#departureCountry").should("be.visible").invoke("val", "France");
