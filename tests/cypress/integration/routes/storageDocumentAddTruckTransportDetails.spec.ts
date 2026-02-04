5feac743569855da6441c022dc60712f8f73764a
Maneesh Kumar Yadav <maneesh.kumar-yadav@capgemini.com>
2026-02-04 04:57:04 +0000

    Add comprehensive bold label tests for all transport types

--- tests/cypress/integration/routes/storageDocumentAddTruckTransportDetails.spec.ts
+++ tests/cypress/integration/routes/storageDocumentAddTruckTransportDetails.spec.ts
@@ -67,6 +67,14 @@ describe("Add Transportation Details Truck: Allowed", () => {
     cy.get('label[id="nationalityOfVehicle-label"]').should("have.class", "govuk-!-font-weight-bold");
     cy.get('label[for="registrationNumber"]').should("have.class", "govuk-!-font-weight-bold");
     cy.get('label[for="freightBillNumber"]').should("have.class", "govuk-!-font-weight-bold");
+
+    // Verify all labels have the base govuk-label class
+    cy.get('label[id="exportedTo-label"]').should("have.class", "govuk-label");
+    cy.get('label[for="pointOfDestination"]').should("have.class", "govuk-label");
+    cy.get('label[for="departurePlace"]').should("have.class", "govuk-label");
+    cy.get('label[id="nationalityOfVehicle-label"]').should("have.class", "govuk-label");
+    cy.get('label[for="registrationNumber"]').should("have.class", "govuk-label");
+    cy.get('label[for="freightBillNumber"]').should("have.class", "govuk-label");
   });
 
   it("should render all required fields for truck departure transport", () => {
