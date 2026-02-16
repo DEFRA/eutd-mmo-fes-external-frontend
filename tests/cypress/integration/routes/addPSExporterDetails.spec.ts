2d43a43db4a8fee75cbc21460496680ec8a913d6
Sal <sal.abuewilly@capgemini.com>
2026-02-16 10:34:29 +0000

    Add tests for EU2026 information display and Welsh translations in exporter details page

--- tests/cypress/integration/routes/addPSExporterDetails.spec.ts
+++ tests/cypress/integration/routes/addPSExporterDetails.spec.ts
@@ -16,6 +16,46 @@ describe("PS: add exporter details page", () => {
     cy.get("[data-testid='change-button']").click({ force: true });
     cy.url().should("include", "/what-exporters-address");
   });
+
+  it("should display EU2026 information text with exclamation icon", () => {
+    // Check for warning text container
+    cy.get(".govuk-warning-text").should("be.visible");
+
+    // Check for exclamation icon
+    cy.get(".govuk-warning-text__icon").should("contain", "!");
+
+    // Check for the specific processing statement information text
+    cy.get(".govuk-warning-text__text").should("contain", "This information will appear on the processing statement.");
+  });
+
+  it("should display 'Company name' label in bold", () => {
+    cy.contains("label", "Company name").should("have.class", "govuk-!-font-weight-bold");
+  });
+
+  it("should display 'Company address' label instead of 'Address'", () => {
+    cy.contains("label", "Company address").should("exist").and("have.class", "govuk-!-font-weight-bold");
+  });
+});
+
+describe("PS: add exporter details page - Welsh translations", () => {
+  const documentUrl = "/create-processing-statement/GBR-2021-PS-8EEB7E123";
+  const pageUrl = `${documentUrl}/add-exporter-details`;
+
+  beforeEach(() => {
+    const testParams: ITestParams = {
+      testCaseId: TestCaseId.PSAddExporterDetailsEmpty,
+    };
+    // Visit with Welsh language
+    cy.visit(pageUrl, { qs: { ...testParams, lng: "cy" } });
+  });
+
+  it("should display Welsh translation for information text", () => {
+    cy.get(".govuk-warning-text__text").should("contain", "Bydd yr wybodaeth yma yn ymddangos ar y datganiad prosesu.");
+  });
+
+  it("should display Welsh translation for 'Company address' label", () => {
+    cy.contains("label", "Cyfeiriad y cwmni").should("exist").and("have.class", "govuk-!-font-weight-bold");
+  });
 });
 
 describe("PS: add exporter details - happy path", () => {
