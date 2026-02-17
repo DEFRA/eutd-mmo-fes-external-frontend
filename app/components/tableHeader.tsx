b9d692c798f5f47e0cda3e5c1b708ce3f733d2af
thefishman-fish <isaac.babalola@capgemini.com>
2026-02-17 17:02:44 +0000

    fix: handle empty header text in TableHeader component

--- app/components/tableHeader.tsx
+++ app/components/tableHeader.tsx
@@ -1,3 +1,5 @@
+import isEmpty from "lodash/isEmpty";
+
 type TableHeaderProps = {
   headersToRender: string[];
 };
@@ -5,11 +7,17 @@ type TableHeaderProps = {
 export const TableHeader = ({ headersToRender }: TableHeaderProps) => (
   <thead className="govuk-table__head">
     <tr className="govuk-table__row" role="row">
-      {headersToRender.map((headerText: string) => (
-        <th scope="col" className="govuk-table__header govuk-font-size" role="columnheader" key={headerText}>
-          {headerText}
-        </th>
-      ))}
+      {headersToRender.map((headerText: string) => {
+        if (isEmpty(headerText)) {
+          return <td scope="col" className="govuk-table__header govuk-font-size" role="columnheader" />;
+        }
+
+        return (
+          <th scope="col" className="govuk-table__header govuk-font-size" role="columnheader" key={headerText}>
+            {headerText}
+          </th>
+        );
+      })}
     </tr>
   </thead>
 );
