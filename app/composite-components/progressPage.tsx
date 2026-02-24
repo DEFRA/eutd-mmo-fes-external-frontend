55d3a439cecbcf71ff81da6155af9ef7d1741f18
Aakash Sharma <aakash.sharma@capgemini.com>
2026-02-24 11:16:47 +0000

    Fix : Sonar issue

--- app/composite-components/progressPage.tsx
+++ app/composite-components/progressPage.tsx
@@ -99,14 +99,17 @@ export const ProgressPageComponent = ({ journey }: ProgressPageType) => {
 
   const getNotificationMsg = (c: string) => {
     const notificationMsgs: string[] = [];
-    if (copyDocumentAcknowledged)
-      notificationMsgs.push(
-        voidDocumentConfirm
-          ? t(translations[journey].notificationIsVoided, { ns: "progress" })
-          : journey === "catchCertificate"
-            ? t(translations[journey].notificationNotVoided, { documentNumber: c, ns: "progress" })
-            : t(translations[journey].notificationNotVoided, { c, ns: "progress" })
-      );
+    if (copyDocumentAcknowledged) {
+      let message: string;
+      if (voidDocumentConfirm) {
+        message = t(translations[journey].notificationIsVoided, { ns: "progress" });
+      } else if (journey === "catchCertificate") {
+        message = t(translations[journey].notificationNotVoided, { documentNumber: c, ns: "progress" });
+      } else {
+        message = t(translations[journey].notificationNotVoided, { c, ns: "progress" });
+      }
+      notificationMsgs.push(message);
+    }
 
     return notificationMsgs.length ? notificationMsgs : [];
   };
