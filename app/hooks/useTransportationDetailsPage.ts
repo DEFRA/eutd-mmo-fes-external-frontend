3d8db24fc148e280a9ccdc39ebc1babf089ce703
thefishman-fish <isaac.babalola@capgemini.com>
2026-03-18 15:49:33 +0000

    feat: add maximum number of container numbers to transportation details handling

--- app/hooks/useTransportationDetailsPage.ts
+++ app/hooks/useTransportationDetailsPage.ts
@@ -3,19 +3,14 @@ import isEmpty from "lodash/isEmpty";
 import { scrollToId, TransportType } from "~/helpers";
 import { useScrollOnPageLoad } from "./useScrollOnPageLoad";
 
-export const getTransportErrorKeys = (transportType: TransportType): string[] => {
-  const containerNumberKeys = [
-    "containerNumbers.0",
-    "containerNumbers.1",
-    "containerNumbers.2",
-    "containerNumbers.3",
-    "containerNumbers.4",
-    "containerNumbers.5",
-    "containerNumbers.6",
-    "containerNumbers.7",
-    "containerNumbers.8",
-    "containerNumbers.9",
-  ];
+export const getTransportErrorKeys = (
+  transportType: TransportType,
+  maximumNumberOfContainerNumbers: number
+): string[] => {
+  const containerNumberKeys = Array.from(
+    { length: maximumNumberOfContainerNumbers },
+    (_, i) => `containerNumbers.${i}`
+  );
 
   switch (transportType) {
     case TransportType.TRUCK:
