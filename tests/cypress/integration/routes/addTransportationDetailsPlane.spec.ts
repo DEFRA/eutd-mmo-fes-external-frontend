e6b67357c88d0669f31085e09261ef96c8fe5b29
fishh <54625901+thefishman-fish@users.noreply.github.com>
2026-03-20 09:57:03 +0000

    fix: update error keys for container numbers in transport error handling (#273)
    * fix: update error keys for container numbers in transport error handling
    
    * fix: update error messages for container identification numbers in transport details

--- tests/cypress/integration/routes/addTransportationDetailsPlane.spec.ts
+++ tests/cypress/integration/routes/addTransportationDetailsPlane.spec.ts
@@ -221,10 +221,9 @@ describe("Add Transportation Details Plane: Container Identification Number Vali
     cy.get('input[name="containerNumbers.0"]').type("ABC123!@#", { force: true });
     cy.get("[data-testid=save-and-continue]").click({ force: true });
     cy.contains("h2", /^There is a problem$/).should("be.visible");
-    cy.contains(
-      "a",
-      /^Enter a shipping container number in the correct format. This must be 11 characters: 3 letters, then U, J, Z or R, then 7 numbers.$/
-    ).should("be.visible");
+    cy.contains("a", /^Container identification number must only contain letters, numbers and spaces.$/).should(
+      "be.visible"
+    );
   });
 
   it("should save successfully when container identification number is not provided", () => {
