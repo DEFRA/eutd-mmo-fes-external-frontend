a2ee2132b66fb587a7b77f9da9320fb64ffefa1f
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-07-24 15:01:49 +0100

    FI0-11386 SonarCloud is reporting on frontend - increase coverage

--- tests/cypress/integration/routes/accessibility.spec.ts
+++ tests/cypress/integration/routes/accessibility.spec.ts
@@ -1,3 +1,5 @@
+import { type ITestParams, TestCaseId } from "~/types";
+
 describe("Accessibility page", () => {
   it("renders the page and key accessibility links", () => {
     cy.visit("/accessibility");
@@ -7,5 +9,17 @@ describe("Accessibility page", () => {
     cy.get('a[href="https://www.gov.uk/help/accessibility-statement"]').should("exist");
     cy.get('a[href^="tel:"]').should("contain.text", "0330 159 1989");
     cy.get('a[href="mailto:dominic.Horsfall@marinemanagement.org.uk"]').should("exist");
+  });
+
+  it("navigates to accessibility via in-app link", () => {
+    const testParams: ITestParams = {
+      testCaseId: TestCaseId.UserAttributes,
+    };
+
+    cy.visit("/cookies", { qs: { ...testParams } });
+    cy.get('a[href="/accessibility"]').first().click();
+
+    cy.url().should("include", "/accessibility");
+    cy.contains("h1", "Accessibility statement").should("be.visible");
   });
 });
