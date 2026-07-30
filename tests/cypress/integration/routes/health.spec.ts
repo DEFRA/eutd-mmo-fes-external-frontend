1bbebe6bb4505210ce79ccc19eb6d935c7ddf24c
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-07-30 07:00:02 +0100

    FI0-11386 SonarCloud is reporting on frontend - increase coverage

--- tests/cypress/integration/routes/health.spec.ts
+++ tests/cypress/integration/routes/health.spec.ts
@@ -13,8 +13,19 @@ describe("Health Page", () => {
     cy.get("[data-testid='client-filter-search-fixture']").should("be.visible");
     cy.get("#client-filter-search").should("have.value", "Cod");
 
+    cy.get("[data-testid='filter-search-reset']").first().trigger("click");
+    cy.get("input#q-filter").should("exist");
+
+    cy.get("[data-testid='client-filter-search-fixture'] [data-testid='filter-search-reset']").trigger("click");
+    cy.get("#client-filter-search").should("exist");
+
     cy.get("[data-testid='coverage-notification']").should("contain.text", "A sample notification message");
     cy.get(".client-mounted-error-summary").should("be.visible");
+
+    cy.contains("summary", "RFMO help").click();
+    cy.contains("details.govuk-details", "RFMO help").within(() => {
+      cy.get("a.govuk-link").should("have.attr", "href").and("include", "fishing-area#rfmo");
+    });
 
     cy.get("#hsa-option-yes").first().check();
     cy.get("#hsa-option-error-no").first().check();
@@ -24,6 +35,10 @@ describe("Health Page", () => {
     cy.get("#gearType").last().select("Drift nets (GN)");
     cy.get("#rfmo").select("NEAFC");
     cy.get("[data-testid='client-rfmo-selector-fixture'] select[name='rfmo']").select("NEAFC");
+
+    cy.get("[data-testid='mount-client-filter-search']").click();
+    cy.get("[data-testid='mount-client-rfmo-selector']").click();
+    cy.get("[data-testid='mount-client-error-summary']").click();
 
     cy.get("[data-testid='coverage-fixtures']").should("exist");
   });
