a003d0280b0846b78be4003ed4e07901cf625051
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-07-31 16:47:12 +0100

    Refactor HighSeasAreaFixture component and update tests for coverage validation

--- tests/cypress/integration/routes/returnUri.spec.ts
+++ tests/cypress/integration/routes/returnUri.spec.ts
@@ -2,6 +2,13 @@
 import { type ITestParams, TestCaseId } from "~/types";
 
 describe("Action function for aad login", () => {
+  const testParams: ITestParams = {
+    testCaseId: TestCaseId.adminLogin,
+  };
+
+  const postBody =
+    "client_id=456&scope=openid&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fopenid%2Freturn&response_mode=form_post";
+
   it("should return status 401(unauthorised) when request param is empty", () => {
     cy.request({
       method: "POST",
@@ -14,18 +21,40 @@ describe("Action function for aad login", () => {
   });
 
   it("should return status 401(unauthorised) when tokenset is empty", () => {
-    const testParams: ITestParams = {
-      testCaseId: TestCaseId.adminLogin,
-    };
     cy.request({
       method: "POST",
       url: "/auth/openid/returnUri",
       qs: { ...testParams },
       failOnStatusCode: false,
-      body: "client_id=456&scope=openid&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fopenid%2Freturn&response_mode=form_post",
+      body: postBody,
     }).as("unauthorised");
 
     cy.get("@unauthorised").should((response) => {
+      expect(response.status).to.eq(401);
+    });
+  });
+
+  it("should consistently return 401 for both empty and populated admin-login payloads", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth/openid/returnUri",
+      qs: { ...testParams },
+      failOnStatusCode: false,
+    }).as("emptyPayload");
+
+    cy.request({
+      method: "POST",
+      url: "/auth/openid/returnUri",
+      qs: { ...testParams },
+      failOnStatusCode: false,
+      body: postBody,
+    }).as("populatedPayload");
+
+    cy.get("@emptyPayload").should((response) => {
+      expect(response.status).to.eq(401);
+    });
+
+    cy.get("@populatedPayload").should((response) => {
       expect(response.status).to.eq(401);
     });
   });
