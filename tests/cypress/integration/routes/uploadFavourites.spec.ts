9764a880e34191acc70696d7f06e8de10db964ff
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-08-21 09:37:15 +0100

    Update error messages to include full name for FAO in English and Welsh translations
    Copilot-Assisted: true

--- tests/cypress/integration/routes/uploadFavourites.spec.ts
+++ tests/cypress/integration/routes/uploadFavourites.spec.ts
@@ -86,7 +86,7 @@ describe("Upload favourites page: when JavaScript is disabled", () => {
   it("should return an error when a species has not been selected", () => {
     cy.get("[data-testid='add-species']").click();
     cy.contains("h2", /^There is a problem$/).should("be.visible");
-    cy.contains("a", /^Enter the common name or FAO code$/).should("be.visible");
+    cy.contains("a", /^Enter the common name or Food and Agriculture Organisation \(FAO\) code$/).should("be.visible");
   });
 
   it("should return an error when a state has not been selected", () => {
@@ -157,7 +157,7 @@ describe("Upload favourites page: validating inputs", () => {
 
     cy.get("[data-testid='add-species']").click();
     cy.contains("h2", /^There is a problem$/).should("be.visible");
-    cy.contains("a", /^Enter the common name or FAO code$/).should("be.visible");
+    cy.contains("a", /^Enter the common name or Food and Agriculture Organisation \(FAO\) code$/).should("be.visible");
   });
 });
 
