4cebddf19b94a672d46f066f387351cf8f33fa83
Senthilkumar T <senthilkumar.t@capgemini.com>
2026-09-04 17:56:03 +0100

    refactor(privacy): simplify Privacy component and remove legacy coverage fixtures from tests
    Copilot-Assisted: true

--- app/routes/privacy-notice.tsx
+++ app/routes/privacy-notice.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
 import { Main } from "~/components";
 import { PrivacyNotice } from "~/composite-components";
-import ProblemWithService from "~/routes/there-is-a-problem-with-the-service";
-import AccessibilityView from "~/components/accessibilityView";
-import { LegacyCoverageFixtures } from "~/routes/auth.openid.returnUri";
 
-const PrivacyNoticePage = () => (
+const Privacy = () => (
   <Main showHelpLink={false}>
-    <div data-testid="privacy-coverage-fixtures">
-      <ProblemWithService />
-      <AccessibilityView />
-      <LegacyCoverageFixtures />
-    </div>
     <PrivacyNotice />
   </Main>
 );
 
-export default PrivacyNoticePage;
+export default Privacy;
